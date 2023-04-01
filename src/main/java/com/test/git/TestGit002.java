059075c92a5db9b1f28959f4aa7ef8b7c1efc728
xiaojie <xiaojie@qq.com>
2023-04-01 21:38:18 +0800

    master改变了代码

--- src/main/java/com/test/git/TestGit002.java
+++ src/main/java/com/test/git/TestGit002.java
@@ -3,5 +3,7 @@ package com.test.git;
 public class TestGit002 {
     public static void main(String[] args) {
         System.out.println("我是TestGit002");
+        System.out.println("我是TestGit003");
+        System.out.println("我是TestGit004");
     }
 }
